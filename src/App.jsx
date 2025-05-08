8c366937b932f1929ba621ba0c42a6635d3cce89
Muhammad-Asif-Ansari <muhammadasifansari101@gmail.com>
2025-05-09 03:46:43 +0500

    redux toolkit and context api

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,5 @@
 import './App.css'
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Login from './pages/Login';
-import Footer from './components/Footer';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Card from './components/Card';
-import {ThemeProvider} from "./contexts/ThemeContext"
 import AppRouter from './config/Router';
 function App() {
   return (
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,18 +7,6 @@ function Footer() {
     <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
       <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
         <ImEdge/>
-        {/* <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          stroke="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
-          viewBox="0 0 24 24"
-        >
-          <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
-        </svg> */}
         <span className="ml-3 text-xl">EShop</span>
       </a>
       <p className="mt-2 text-sm text-gray-500">
