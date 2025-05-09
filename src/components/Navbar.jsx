0a1de235a4314f2db3612a7008e6df7468b5a4e9
Muhammad-Asif-Ansari <muhammadasifansari101@gmail.com>
2025-05-09 09:42:49 +0500

    navbar implement

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,16 +12,10 @@ function Navbar() {
   return (
     <header className="text-gray-600 body-font">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+        <a className="flex title-font font-medium items-center text-gray-700 mb-4 md:mb-0">
           <ImEdge />
           <span className="ml-3 text-xl">EShop</span>
         </a>
-        {/* <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-          <a className="mr-5 hover:text-gray-900 cursor-pointer">Home</a>
-          <a className="mr-5 hover:text-gray-900 cursor-pointer">About</a>
-          <a className="mr-5 hover:text-gray-900 cursor-pointer">Contact Us</a>
-          <a className="mr-5 hover:text-gray-900 cursor-pointer">Login</a>
-        </nav> */}
      <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center gap-4">
           <Link to="/" className="hover:text-gray-900">Home</Link>
           <Link to="/about" className="hover:text-gray-900">About</Link>
